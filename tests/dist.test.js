c55c33b1e4dc5d8bbe85df205ce9f580f89ebc07
actionchain <actionchain@chainguard.dev>
2026-05-12 22:50:49 +0000

    Update anchore/scan-action@v7.3.0

---
+++ tests/dist.test.js
@@ -0,0 +1,76 @@
+const child_process = require("child_process");
+const path = require("path");
+
+// these are smoke tests that the dist build was created correctly,
+// functional tests should be in action.test.js
+describe("scan-action dist build", () => {
+  it("runs download-grype", () => {
+    const { stdout } = runDistBuild({
+      run: "download-grype",
+      sbom: "fixtures/test_sbom.spdx.json", // should be ignored
+    });
+    expect(stdout).toContain("Downloaded Grype");
+    expect(stdout).not.toContain("Failed minimum severity level.");
+  });
+
+  it("fails due to vulnerabilities found", () => {
+    const { stdout } = runDistBuild({
+      image:
+        "anchore/test_images:vulnerabilities-debian-56d52bc@sha256:7ed765e2d195dc594acc1c48fdda0daf7a44026cfb42372544cae1909de22adb",
+    });
+    expect(stdout).toContain("Failed minimum severity level.");
+  });
+
+  it("runs with sbom", () => {
+    const { stdout } = runDistBuild({
+      sbom: "tests/fixtures/test_sbom.spdx.json",
+    });
+    expect(stdout).toContain("Failed minimum severity level.");
+  });
+});
+
+// Execute the action, and return any outputs
+function runDistBuild(inputs) {
+  const repoRootDir = path.dirname(__dirname);
+  const distPath = path.join(repoRootDir, "dist", "index.js");
+
+  // Set up the environment variables
+  const env = {
+    HOME: process.env.HOME,
+    PATH: process.env.PATH,
+    // RUNNER_DEBUG: "1", // uncomment for debug logging
+    RUNNER_TEMP: process.env.RUNNER_TEMP,
+    RUNNER_TOOL_CACHE: process.env.RUNNER_TOOL_CACHE,
+    GRYPE_DB_AUTO_UPDATE: "false",
+    GRYPE_DB_VALIDATE_AGE: "false",
+    GRYPE_DB_VALIDATE_BY_HASH_ON_START: "false",
+    GRYPE_DB_REQUIRE_UPDATE_CHECK: "false",
+    GRYPE_DB_MAX_ALLOWED_BUILT_AGE: "8760h", // 1 year
+  };
+  // this is brittle and may need to be updated, but is currently how input are passed to the process:
+  // reverse core.js: const val = process.env[`INPUT_${name.replace(/ /g, '_').toUpperCase()}`] || '';
+  for (const k in inputs) {
+    // NOTE: there is a bug with node exec where environment variables with dashes
+    // are not always preserved - we will just have to rely on defaults in the code
+    env[`INPUT_${k}`.toUpperCase()] = inputs[k];
+  }
+
+  // capture stdout and exit code, and execute the command
+  let exitCode = 0;
+  let stdout;
+  try {
+    stdout = child_process
+      .execSync(`node ${distPath}`, {
+        env,
+      })
+      .toString("utf8");
+  } catch (error) {
+    exitCode = error.status;
+    stdout = error.stdout.toString("utf8");
+  }
+
+  return {
+    exitCode,
+    stdout,
+  };
+}
---
+++ tests/mocks.js
@@ -0,0 +1,120 @@
+const githubActionsCore = require("@actions/core");
+const fs = require("fs");
+const path = require("path");
+const os = require("os");
+const process = require("process");
+
+const cleanups = [];
+
+module.exports = {
+  onCleanup(fn) {
+    cleanups.push(fn);
+  },
+
+  cleanup() {
+    for (let fn = cleanups.pop(); fn; fn = cleanups.pop()) {
+      fn();
+    }
+    jest.restoreAllMocks();
+  },
+
+  mock(lib, overrides) {
+    for (const name of Object.keys(overrides)) {
+      module.exports.onCleanup(
+        jest.spyOn(lib, name).mockImplementation(overrides[name]).mockRestore,
+      );
+    }
+  },
+
+  mockIO(inputs) {
+    const outputs = {};
+    module.exports.mock(githubActionsCore, {
+      getInput(name) {
+        return inputs[name];
+      },
+      setOutput(name, value) {
+        outputs[name] = value;
+      },
+      // ignore setFailed calls that set process.exitCode due to https://github.com/jestjs/jest/issues/14501
+      setFailed() {},
+    });
+    return outputs;
+  },
+
+  setEnv(env) {
+    const originalValues = {};
+    for (const k of Object.keys(env)) {
+      if (k in process.env) {
+        originalValues[k] = process.env[k];
+      }
+      process.env[k] = env[k];
+    }
+    module.exports.onCleanup(() => {
+      for (const k of Object.keys(env)) {
+        if (k in originalValues) {
+          process.env[k] = originalValues[k];
+        } else {
+          delete process.env[k];
+        }
+      }
+    });
+  },
+
+  tmpdir() {
+    const dir = fs.mkdtempSync(path.join(os.tmpdir(), "scan-action-test-"));
+    module.exports.onCleanup(() => {
+      if (fs.existsSync(dir)) {
+        fs.rmSync(dir, { recursive: true });
+      }
+    });
+    return dir;
+  },
+
+  // runAction mocks input with the provided input, captures stdout,
+  // sets test db environment variables, and runs the action
+  async runAction(inputs) {
+    const outputs = module.exports.mockIO(inputs);
+
+    const { run } = require("../index");
+
+    let failure = "";
+    let stdout = "";
+
+    const append = (...args) => {
+      stdout += args.join(" ") + "\n";
+    };
+
+    module.exports.setEnv({
+      GRYPE_DB_AUTO_UPDATE: "false",
+      GRYPE_DB_VALIDATE_AGE: "false",
+    });
+
+    module.exports.mock(githubActionsCore, {
+      error: append,
+      info: append,
+      debug: append,
+      warning: append,
+      setFailed: (...args) => {
+        append(...args);
+        failure = args.join(" ");
+      },
+    });
+
+    module.exports.mock(console, {
+      log: append,
+      error: append,
+      warn: append,
+      info: append,
+      debug: append,
+      trace: append,
+    });
+
+    await run();
+
+    return {
+      stdout,
+      failure,
+      outputs,
+    };
+  },
+};
